9925e720c72ffc3d859776bd9275e0c7f401ce46
Erich Wellinger <ewellinger@icloud.com>
2015-12-22 15:38:31 -0700

    Reworked the topic coherence function to operate much faster

--- cluster.py
+++ cluster.py
@@ -14,6 +14,9 @@ def top_words(clusters, feature_names, num_words):
 
 
 def cluster_headlines(df, labels, kmeans, num_rows=None, random=False):
+    '''
+    You should probably get rid of this function...
+    '''
     if num_rows:
         if random:
             idxs = [np.random.choice(df.loc[labels == label, :].index, size=num_rows) for label in xrange(kmeans.n_clusters)]
@@ -39,7 +42,8 @@ def create_document_vector(df, max_features=5000, max_df=1, min_df=1):
     tfid = TfidfVectorizer(input='content', stop_words=stopwords, use_idf=True, lowercase=True, max_features=max_features)
     X = tfid.fit_transform(df['lemmatized_text'].values)
     feature_names = np.array(tfid.get_feature_names())
-    return tfid, X, feature_names
+    reverse_lookup = {word: idx for idx, word in enumerate(feature_names)}
+    return tfid, X, feature_names, reverse_lookup
 
 
 def cluster_articles(df, n_clusters, max_features=5000, max_df=1, min_df=1,  num_words=15, num_headlines=None):
@@ -54,7 +58,7 @@ def cluster_articles(df, n_clusters, max_features=5000, max_df=1, min_df=1,  num
 
 
 def nmf_articles(df, n_topics, n_features=5000, n_top_words=20, random_state=None, max_df=1, min_df=1):
-    tfid, X, feature_names = create_document_vector(df, max_features=n_features, max_df=max_df, min_df=min_df)
+    tfid, X, feature_names, reverse_lookup = create_document_vector(df, max_features=n_features, max_df=max_df, min_df=min_df)
 
     nmf = NMF(n_components=n_topics, random_state=random_state, alpha=.1, l1_ratio=0.5).fit(X)
     W = nmf.transform(X)
@@ -64,10 +68,10 @@ def nmf_articles(df, n_topics, n_features=5000, n_top_words=20, random_state=Non
     rel_importance = np.array([row[np.argmax(row)] / row.sum() for row in W])
     words = top_words(nmf.components_, feature_names, n_top_words)
 
-    return tfid, nmf, X, W, labels, rel_importance, words, feature_names
+    return tfid, nmf, X, W, labels, rel_importance, words, feature_names, reverse_lookup
 
 
-def topic_coherence(df, topic_words, e=0.01, num_top_words=10):
+def topic_coherence(X, reverse_lookup, topic_words, e=0.01, num_top_words=10):
     '''
     Using the UMass topic coherence as definied in http://anthology.aclweb.org/D/D12/D12-1087.pdf
     '''
@@ -78,15 +82,32 @@ def topic_coherence(df, topic_words, e=0.01, num_top_words=10):
     while True:
         try:
             word1, word2 = perm.next()
-            result += coherence_score(df, word1, word2, e)
+            result += coherence_score(X, reverse_lookup, word1, word2, e)
         except:
             return result
 
 
-def coherence_score(df, word1, word2, e):
-    return np.log((len(df[df['lemmatized_text'].str.contains(word1) & df['lemmatized_text'].str.contains(word2)]) + e) / df['lemmatized_text'].str.contains(word2).sum())
+def coherence_score(X, reverse_lookup, word1, word2, e):
+    # return np.log((len(df[df['lemmatized_text'].str.contains(word1) & df['lemmatized_text'].str.contains(word2)]) + e) / df['lemmatized_text'].str.contains(word2).sum())
+    return np.log((document_word_count(X, reverse_lookup, word1, word2) + e) / document_word_count(X, reverse_lookup, word2))
 
 
+def document_word_count(X, reverse_lookup, word1, word2=None):
+    '''
+    INPUTS: X - tfidf matrix
+            reverse_lookup - Dictionary relating word to the cooresponding column in the tfidf matrix
+            word1 - First word to search for
+            word2 - Second word to search for.
+    OUTPUT: Int - Number of documents in the corpus that have either word1 or word1 AND word2
+    '''
+    if word2:
+        return len(np.where((X[:, reverse_lookup[word1]] > 0).toarray() & (X[:, reverse_lookup[word2]] > 0).toarray())[0])
+    else:
+        return len(np.where((X[:, reverse_lookup[word1]] > 0).toarray())[0])
+
+
+def sum_squared_err(nmf, X, labels):
+    return np.sum([np.linalg.norm(X[idx] - nmf.components_[labels[idx]])**2 for idx in xrange(len(X))])
 
 
 if __name__=='__main__':
@@ -94,8 +115,8 @@ if __name__=='__main__':
 
     # tfid, kmeans, labels, top_words, headlines = cluster_articles(df, n_clusters=14, max_features=15000, num_words=20, max_df=0.95, min_df=2)
 
-    # tfid, nmf, X, W, labels, rel_importance, topic_words, feature_names = nmf_articles(df, n_topics=14, n_features=15000, random_state=1, max_df=0.95, min_df=2)
-    #
+    # tfid, nmf, X, W, labels, rel_importance, topic_words, feature_names, reverse_lookup = nmf_articles(df, n_topics=20, n_features=15000, random_state=1, max_df=0.95, min_df=2)
+    # print 'Clustering Done...'
     # coherence = []
     # for idx, words in enumerate(topic_words):
     #     num_articles = len(labels[labels == idx])
@@ -116,10 +137,86 @@ if __name__=='__main__':
     I'm going to try looking over many different number of latent topics and see what that does to the average topic coherence for each model.
     '''
     avg_coherence = []
-    for n_topics in xrange(12, 51):
-        print '{} Number of Topics Processing...'.format(n_topics)
-        tfid, nmf, X, W, labels, rel_importance, topic_words, feature_names = nmf_articles(df, n_topics=n_topics, n_features=15000, random_state=1, max_df=0.95, min_df=2)
+    n_topics = range(5, 50, 5)
+    for n_topic in n_topics:
+        print '{} Topics Processing...'.format(n_topic)
+        tfid, nmf, X, W, labels, rel_importance, topic_words, feature_names, reverse_lookup = nmf_articles(df, n_topics=n_topic, n_features=15000, random_state=1, max_df=0.95, min_df=2)
+        print 'Factorizing Done...'
         coherence = []
         for words in topic_words:
-            coherence.append(topic_coherence(df, words))
+            coherence.append(topic_coherence(X, reverse_lookup, words))
         avg_coherence.append(np.mean(coherence))
+        print 'Avg Coherence Done...\n'
+
+
+# Topics 12 through 180
+# topics = range(12, 51) + range(52, 81, 2) + range(85, 131, 5) + range(140, 181, 10)
+# avg_coherence = [-84.271885872677089,
+#  -85.056773895715168,
+#  -87.207059792746008,
+#  -91.283108150538908,
+#  -89.059628926444248,
+#  -91.46036413392369,
+#  -91.550035822549248,
+#  -93.139844012185804,
+#  -91.925732728396525,
+#  -94.358775033486495,
+#  -96.502742506711556,
+#  -99.034741565714825,
+#  -98.702254274317454,
+#  -97.888423898130767,
+#  -102.34395794737331,
+#  -103.28015262557012,
+#  -101.2634091458153,
+#  -100.66480423304868,
+#  -103.58547924985452,
+#  -106.11057939146643,
+#  -103.16565988091494,
+#  -105.35513697221467,
+#  -104.70601108947588,
+#  -105.75473662540017,
+#  -106.41060258426161,
+#  -107.63233743517524,
+#  -108.74751978047732,
+#  -107.464707449849,
+#  -108.48114425809096,
+#  -109.49846022549401,
+#  -111.96143447802491,
+#  -113.19636727001493,
+#  -112.57067058821157,
+#  -110.03253093124079,
+#  -113.41379511011189,
+#  -112.62906674255117,
+#  -112.87709413012958,
+#  -115.2142664806567,
+#  -115.40772103489027,
+#  -116.78368387828597,
+#  -114.86973907533985,
+#  -116.97142224663536,
+#  -117.62904294595,
+#  -117.98007215098878,
+#  -118.89041999425125,
+#  -117.72868611106972,
+#  -116.63255904999869,
+#  -119.49797922388851,
+#  -119.44894180227961,
+#  -121.32148328790004,
+#  -120.26321591188591,
+#  -118.58180592403561,
+#  -122.11598722751644,
+#  -122.53031004443876,
+#  -121.82856623908583,
+#  -123.21921697489067,
+#  -123.6055355024322,
+#  -126.65551586482486,
+#  -125.8272888476405,
+#  -128.88429861511943,
+#  -138.92294342345417,
+#  -140.28479208979368,
+#  -144.14415983182496,
+#  -150.9131725691125,
+#  -185.53675513797452,
+#  -186.45098435435264,
+#  -219.04478511586885,
+#  -244.31768424636127,
+#  -274.81272634049697]
