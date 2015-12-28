28323b6edf07bcc483f140cf09b6b96c80273ab8
Erich Wellinger <ewellinger@icloud.com>
2015-12-28 15:16:13 -0700

    Added function for visualizing article count / freq over time

--- plots.py
+++ plots.py
@@ -22,8 +22,63 @@ def plot_candidate_percentages(df, candidates):
     plt.show()
 
 
+def count_articles_by_time(df, searchterm=None, source=False, freq='W', normalize=False, show=True):
+    if source:
+        outlets = [('nyt', 'New York Times'), ('foxnews', 'Fox News'), ('npr', 'NPR'), ('guardian', 'The Guardian'), ('wsj', 'Wall Street Journal')]
+    if not searchterm and not source:
+        ts = pd.Series([1], index=df['date_published']).resample(freq, how='sum')
+        fig = plt.figure(figsize=(12, 8))
+        plt.subplots_adjust(left=0.08, bottom=0.12, right=0.95, top=0.92)
+        ts.plot()
+        plt.xlabel('Date Published')
+        plt.ylabel('Article Count (freq={})'.format(freq))
+    elif not searchterm and source:
+        timeseries = [pd.Series([1], index=df.loc[df['source'] == outlet, 'date_published']).resample(freq, how='sum') for outlet in zip(*outlets)[0]]
+        if normalize:
+            timeseries = [ts / len(df.loc[df['source'] == outlet]) for ts, outlet in zip(timeseries, zip(*outlets)[0])]
+        fig = plt.figure(figsize=(12, 8))
+        plt.subplots_adjust(left=0.08, bottom=0.12, right=0.95, top=0.92)
+        for idx, ts in enumerate(timeseries):
+            ts.plot(label=outlets[idx][1])
+        plt.xlabel('Date Published', fontsize=12)
+        if normalize:
+            plt.ylabel('Article Frequency (freq = {})'.format(freq), fontsize=12)
+        else:
+            plt.ylabel('Article Count (Freq = {})'.format(freq), fontsize=12)
+        plt.legend(loc='best')
+    elif searchterm and not source:
+        ts = pd.Series(df['lemmatized_text'].str.contains(searchterm).astype('int').values, index=df['date_published']).resample(freq, how='sum')
+        fig = plt.figure(figsize=(12, 8))
+        plt.subplots_adjust(left=0.08, bottom=0.12, right=0.95, top=0.92)
+        ts.plot()
+        plt.xlabel('Date Published', fontsize=12)
+        plt.ylabel('Article Count (freq={})'.format(freq), fontsize=12)
+        plt.title("Articles Containing '{}'".format(searchterm), fontsize=14)
+    elif searchterm and source:
+        timeseries = [pd.Series(df.loc[df['source'] == outlet, 'lemmatized_text'].str.contains(searchterm).astype('int').values, index=df.loc[df['source'] == outlet, 'date_published']).resample(freq, how='sum') for outlet in zip(*outlets)[0]]
+        if normalize:
+            timeseries = [ts / len(df.loc[df['source'] == outlet]) for ts, outlet in zip(timeseries, zip(*outlets)[0])]
+        fig = plt.figure(figsize=(12, 8))
+        plt.subplots_adjust(left=0.08, bottom=0.12, right=0.95, top=0.92)
+        for idx, ts in enumerate(timeseries):
+            ts.plot(label=outlets[idx][1])
+        plt.xlabel('Date Published', fontsize=12)
+        if normalize:
+            plt.ylabel('Article Frequency (freq = {})'.format(freq), fontsize=12)
+        else:
+            plt.ylabel('Article Count (Freq = {})'.format(freq), fontsize=12)
+        plt.legend(loc='best')
+        plt.title("Articles Containing '{}'".format(searchterm), fontsize=14)
+        plt.legend(loc='best')
+    if show:
+        plt.show()
+
+
+
+
+
 if __name__=='__main__':
     df = pd.read_pickle('election_data.pkl')
 
     # Plot % of articles mentioning candidate accross all news sources
-    plot_candidate_percentages(df, ['Clinton', 'Trump', 'Bush'])
+    # plot_candidate_percentages(df, ['Clinton', 'Trump', 'Bush'])
