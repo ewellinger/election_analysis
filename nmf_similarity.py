9f8835b04ce5527cc6995422409ebeb64e4dcb1f
Erich Wellinger <ewellinger@icloud.com>
2015-12-30 16:36:03 -0700

    Reworked code to actually calculate cosine similarity and not cosine distance

--- nmf_similarity.py
+++ nmf_similarity.py
@@ -16,13 +16,13 @@ def nmf_similarity(df, num_topics):
     for topic in pbar(xrange(num_topics)):
         # When looking at high numbers of topics, it is possible for no points to be assigned to that topic, in which case pairwise_distances() will throw an error
         if len(labels[labels == topic]):
-            cosine_sim = pairwise_distances(X[labels == topic], metric='cosine', n_jobs=-1)
-            idx = np.tril_indices(cosine_sim.shape[0], k=-1)
-            tfidf_similarity.append(cosine_sim[idx].mean())
+            cosine_dist = pairwise_distances(X[labels == topic], metric='cosine', n_jobs=-1)
+            idx = np.tril_indices(cosine_dist.shape[0], k=-1)
+            tfidf_similarity.append(1 - cosine_dist[idx].mean())
     topic_similarity = pairwise_distances(nmf.components_, metric='cosine', n_jobs=1)
     idx = np.tril_indices(topic_similarity.shape[0], k=-1)
     print '\n'
-    return np.mean(tfidf_similarity), topic_similarity[idx].mean()
+    return np.mean(tfidf_similarity), 1 - topic_similarity[idx].mean(), nmf.reconstruction_err_
 
 
 def make_similarity_plot(n_topics, tfidf_similarity, topic_similarity):
@@ -39,15 +39,16 @@ def make_similarity_plot(n_topics, tfidf_similarity, topic_similarity):
     plt.title('Articles Within Topics', fontsize=11)
     plt.suptitle('Cosine Similarity of NMF Model', fontsize=18, x=0.52)
     plt.subplots_adjust(left=0.09, right=0.95, top=0.87, hspace=0.16)
-    plt.savefig('nmf_similarity.png', dpi=350)
+    plt.savefig('plots/nmf_similarity2.png', dpi=350)
 
 
 if __name__=='__main__':
     df = pd.read_pickle('election_data.pkl')
 
-    n_topics = range(2, 41) + range(42, 161, 2)
+    n_topics = range(2, 41) + range(42, 101, 2) + range(105, 161, 5)
     similarity = [nmf_similarity(df, n_topic) for n_topic in n_topics]
     tfidf_similarity = np.array(zip(*similarity)[0])
     topic_similarity = np.array(zip(*similarity)[1])
+    reconst_err = np.array(zip(*similarity)[2])
 
     make_similarity_plot(n_topics, tfidf_similarity, topic_similarity)
